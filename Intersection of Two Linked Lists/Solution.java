5a0aef2d5b2a770d9ea1cf17e32914b6abb17764
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-03-18 12:35:10 +0530

    Commit 33

---
+++ Intersection of Two Linked Lists/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Map<ListNode,Integer> m = new HashMap<>();
+        while(headA!=null){
+            m.put(headA,1);
+            headA = headA.next;
+        }
+        while(headB!=null){
+            if(m.get(headB)!=null){
+                if(m.get(headB)==1){
+                    return headB;
+                }
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
