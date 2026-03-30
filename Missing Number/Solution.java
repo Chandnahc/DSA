764dc7e805fb7f2828f39736717043b5be47fda6
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-03-30 17:53:50 +0530

    Commit 59

---
+++ Missing Number/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int l = nums.length;
+        int sum = 0;
+        for(int i=0;i<l;i++){
+            sum += nums[i];
+        }
+        return (l*(l+1)/2)-sum;
+    }
+}
