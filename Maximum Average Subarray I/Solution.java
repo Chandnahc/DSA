9f92e7bbabfd9d43765fe7b9acb5fbbdd83ca818
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-05-19 10:00:41 +0530

    Commit 143

---
+++ Maximum Average Subarray I/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int maxSum = 0;
+        for(int i=0;i<k;i++){
+            maxSum += nums[i];
+        }
+        int currentSum = maxSum;
+        for(int i=k;i<nums.length;i++){
+            currentSum += nums[i] - nums[i-k];
+            if(currentSum > maxSum){
+                maxSum = currentSum;
+            }
+        }
+        return (double)maxSum/k;
+    }
+}
