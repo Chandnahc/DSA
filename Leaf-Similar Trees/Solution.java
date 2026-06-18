f0af97c9d433c440d6d8cc98f1ad3d80475ae6e4
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-06-18 13:22:19 +0530

    Commit 177

---
+++ Leaf-Similar Trees/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        addLeaf(root1,l1);
+        addLeaf(root2,l2);
+        if(l1.size()!=l2.size()) return false;
+        for(int i=0;i<l1.size();i++){
+            if(!l1.get(i).equals(l2.get(i))) return false;
+        }
+        return true;
+
+    }
+
+    public void addLeaf(TreeNode root,List<Integer> l){
+        if(root == null) return;
+        if(root.left == null && root.right==null) l.add(root.val);
+        addLeaf(root.left,l);
+        addLeaf(root.right,l);
+    }
+}
