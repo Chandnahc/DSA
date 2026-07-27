cb48842872b666a6df90c9861031c01cd7cf2976
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-07-27 23:16:39 +0530

    Commit 227

---
+++ Number of Steps to Reduce a Number in Binary Representation to One/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSteps(String s) {
+        int steps = 0;
+        int carry = 0;
+
+        for (int i = s.length() - 1; i > 0; i--) {
+            int bit = (s.charAt(i) - '0') + carry;
+
+            if (bit == 1) {
+                // odd: +1 and /2
+                steps += 2;
+                carry = 1;
+            } else {
+                // even: only /2
+                steps += 1;
+            }
+        }
+
+        return steps + carry;
+    }
+}
