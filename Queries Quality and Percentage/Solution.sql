90ed8553eddd4471e5763c912396ba7ce1efa71f
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-08-10 09:30:12 +0530

    Commit 261

---
+++ Queries Quality and Percentage/Solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select query_name, ROUND(AVG(rating/position),2) as quality, ROUND((SUM(case when rating<3 then 1 else 0 end)/count(*))*100,2) as poor_query_percentage from Queries group by query_name;
