fe3e159c8fb97b435c1449200ffbf6a5ffd283a3
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-08-13 10:48:25 +0530

    Commit 267

---
+++ Minimum Time Visiting All Points/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int count = 0;
+        for(int i=1;i<points.length;i++){
+            int m = Math.abs(points[i][0] - points[i-1][0]);
+            int n = Math.abs(points[i][1] - points[i-1][1]);
+            count += Math.max(m,n);
+        }
+        return count;
+    }
+}
