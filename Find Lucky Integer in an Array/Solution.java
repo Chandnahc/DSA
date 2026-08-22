a5aa1d067afb38371163f3a8215815266effa191
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-08-22 11:10:25 +0530

    Commit 301

---
+++ Find Lucky Integer in an Array/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int[] freq = new int[501];
+        for(int i:arr){
+            freq[i]++;
+        }
+        int max = -1;
+        for(int i=1;i<501;i++){
+            if(freq[i]==i){
+                max = i;
+            }
+        }
+        return max;
+    }
+}
