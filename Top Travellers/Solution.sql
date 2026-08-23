217a651a1ae76f053a7d8e3509d3a38bb61a10af
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-08-23 20:08:05 +0530

    Commit 304

---
+++ Top Travellers/Solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select u.name, sum(case when r.distance is not null then r.distance else 0 END) as travelled_distance from Users u left join Rides r on u.id=r.user_id group by u.id order by travelled_distance desc, u.name;
