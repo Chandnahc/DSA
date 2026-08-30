5567befb5936ed5837765816876ae2d5eedbb1a9
Chandresh Carpenter <71308203+Chandnahc@users.noreply.github.com>
2026-08-30 22:40:41 +0530

    Commit 321

---
+++ Make Two Arrays Equal by Reversing Subarrays/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] freq = new int[1001];
+        for(int i:target){
+            freq[i]++;
+        }
+        for(int i:arr){
+            if(freq[i]==0) return false;
+            freq[i]--;
+        }
+        return true;
+    }
+}
